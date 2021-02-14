5250d45ab057f07f3d238ea6c7dd5e447c79c178
SirPeace <roman.khabibulin12@gmail.com>
2021-02-14 18:30:37 +0300

    Make timer buttons styles simillar to Pomofocus

--- src/components/Timer/TimerControls/TimerButton/TimerButton.jsx
+++ src/components/Timer/TimerControls/TimerButton/TimerButton.jsx
@@ -13,6 +13,7 @@ function TimerButton({
 }) {
   const classes = useStyles()
   const appliedClasses = [classes.button]
+  const appliedShadowClasses = [classes.shadow]
 
   const [btnLock, setBtnLock] = React.useState(false)
 
@@ -36,14 +37,22 @@ function TimerButton({
   // Set proper class for each button type
   if (type === "start") {
     appliedClasses.push(classes.startButton)
+    if (session === "short_break") {
+      appliedShadowClasses.push(classes.shadow_short_break)
+    } else if (session === "long_break") {
+      appliedShadowClasses.push(classes.shadow_long_break)
+    }
   } else if (type === "pause") {
     appliedClasses.push(classes.pauseButton)
+    appliedShadowClasses.push(classes.shadow_pause)
   } else if (type === "stop") {
     appliedClasses.push(classes.stopButton)
+    appliedShadowClasses.push(classes.shadow_stop)
   }
 
   return (
-    <div className={classes.shell}>
+    <div className={classes.body}>
+      <div className={appliedShadowClasses.join(" ")}></div>
       <Button
         disabled={btnLock}
         variant="contained"
