1ae37dd8e259687a4fdb13cf48243ecf9571c624
SirPeace <roman.khabibulin12@gmail.com>
2021-02-16 19:05:41 +0300

    Remove semicolons

--- src/libs/functions.js
+++ src/libs/functions.js
@@ -5,18 +5,18 @@
  * @returns {string | false} String of format "mm:ss"
  */
 export function getTimeString(value, unit = "ms") {
-  if (unit === "time") return value;
-  const ms = toMs(value, unit);
+  if (unit === "time") return value
+  const ms = toMs(value, unit)
 
-  let s = ms / 1000;
-  let m = Math.floor(s / 60);
+  let s = ms / 1000
+  let m = Math.floor(s / 60)
 
-  s -= m * 60;
+  s -= m * 60
 
-  if (m < 10) m = "0" + m;
-  if (s < 10) s = "0" + s;
+  if (m < 10) m = "0" + m
+  if (s < 10) s = "0" + s
 
-  return `${m}:${s}`;
+  return `${m}:${s}`
 }
 
 /**
@@ -26,14 +26,14 @@ export function getTimeString(value, unit = "ms") {
  * @returns {number|boolean} Milliseconds from given unit or false if translation unit is not supported
  */
 export function toMs(value, unit) {
-  if (unit === "m") return value * 60 * 1000;
-  if (unit === "s") return value * 1000;
-  if (unit === "ms") return value;
+  if (unit === "m") return value * 60 * 1000
+  if (unit === "s") return value * 1000
+  if (unit === "ms") return value
   if (unit === "time") {
-    let [m, s] = value.split(":", 2);
-    m = m.startsWith("0") ? +m[1] : +m;
-    s = s.startsWith("0") ? +s[1] : +s;
-    return m * 60 * 1000 + s * 1000;
+    let [m, s] = value.split(":", 2)
+    m = m.startsWith("0") ? +m[1] : +m
+    s = s.startsWith("0") ? +s[1] : +s
+    return m * 60 * 1000 + s * 1000
   }
-  return false;
+  return false
 }
